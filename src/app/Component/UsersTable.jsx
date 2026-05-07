4fe5273305609d9a51e2b464dc162762901dfd87
Sakibur59 <sakiburrahman5978@gmail.com>
2026-05-07 11:33:50 +0600

    delete heroui alertlog added

--- src/app/Component/UsersTable.jsx
+++ src/app/Component/UsersTable.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button, Table } from "@heroui/react";
+import { AlertDialog, Button, Table } from "@heroui/react";
 import Link from "next/link";
 
 const UsersTable = ({ users }) => {
@@ -20,9 +20,43 @@ const UsersTable = ({ users }) => {
                 <Table.Cell>{user.role}</Table.Cell>
                 <Table.Cell>{user.email}</Table.Cell>
                 <Table.Cell>
-                    <Link href={`/users/${user._id}`}><Button variant="outline">Details</Button></Link>
-                    <Link href={`/users/${user._id}/edit`}><Button variant="outline">Edit</Button></Link>
+                  <Link href={`/users/${user._id}`}>
+                    <Button variant="outline">Details</Button>
+                  </Link>
+                  <Link href={`/users/${user._id}/edit`}>
+                    <Button variant="outline">Edit</Button>
+                  </Link>
+                  <AlertDialog>
                     <Button variant="danger">Delete</Button>
+                    <AlertDialog.Backdrop>
+                      <AlertDialog.Container>
+                        <AlertDialog.Dialog className="sm:max-w-[400px]">
+                          <AlertDialog.CloseTrigger />
+                          <AlertDialog.Header>
+                            <AlertDialog.Icon status="danger" />
+                            <AlertDialog.Heading>
+                              Delete User permanently?
+                            </AlertDialog.Heading>
+                          </AlertDialog.Header>
+                          <AlertDialog.Body>
+                            <p>
+                              This will permanently delete{" "}
+                              <strong>{user.name}</strong> and all of its
+                              data. This action cannot be undone.
+                            </p>
+                          </AlertDialog.Body>
+                          <AlertDialog.Footer>
+                            <Button slot="close" variant="tertiary">
+                              Cancel
+                            </Button>
+                            <Button slot="close" variant="danger">
+                              Confirm Delete
+                            </Button>
+                          </AlertDialog.Footer>
+                        </AlertDialog.Dialog>
+                      </AlertDialog.Container>
+                    </AlertDialog.Backdrop>
+                  </AlertDialog>
                 </Table.Cell>
               </Table.Row>
             ))}
