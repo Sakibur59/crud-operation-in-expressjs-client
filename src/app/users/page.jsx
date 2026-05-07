cdd1d16fb84e4e6b1fd5fa42309985278f59108d
Sakibur59 <sakiburrahman5978@gmail.com>
2026-05-07 12:11:17 +0600

    add a user modal added

--- src/app/users/page.jsx
+++ src/app/users/page.jsx
@@ -2,12 +2,16 @@ import React from 'react';
 import { getUsers } from '../lib/data';
 import UsersTable from '../Component/UsersTable';
 import { deleteUser } from '../lib/actions';
+import AddUserModal from '../Component/AddUserModal';
 
 const UserPage = async() => {
     const users = await getUsers();
     return (
         <div>
+            <div className='flex gap-2'>
             <h1>User Management: {users.length}</h1>
+                <AddUserModal></AddUserModal>
+            </div>
             <UsersTable users={users} deleteUserAction={deleteUser}></UsersTable>
         </div>
     );
