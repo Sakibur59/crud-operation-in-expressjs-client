fd8c8e427a44bbf0d20f6680d777dd7b9aa2df31
Sakibur59 <sakiburrahman5978@gmail.com>
2026-05-08 22:30:21 +0600

    added some style in user details pages

--- src/app/users/[usersId]/page.jsx
+++ src/app/users/[usersId]/page.jsx
@@ -1,16 +1,18 @@
-import { getUserById } from '@/app/lib/data';
-import React from 'react';
+import { getUserById } from "@/app/lib/data";
+import React from "react";
 
-const UserDetailsPage = async({params}) => {
-    const {usersId} = await params;
-    const user = await getUserById(usersId);
-    return (
-        <div>
-            <h2>User details:{user.name}</h2>
-            <p>Email: {user.email}</p>
-            <p>Role: {user.role}</p>
-        </div>
-    );
+const UserDetailsPage = async ({ params }) => {
+  const { usersId } = await params;
+  const user = await getUserById(usersId);
+  return (
+    <div className=" mt-20">
+      <div className="border-2 w-100 mx-auto p-10">
+        <h2 className="font-bold text-blue-500">User details:{user.name}</h2>
+        <p className="font-bold text-blue-500">Email: {user.email}</p>
+        <p className="font-bold text-blue-500">Role: {user.role}</p>
+      </div>
+    </div>
+  );
 };
 
 export default UserDetailsPage;
